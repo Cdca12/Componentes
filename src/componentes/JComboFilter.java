4ff4cb6667ac6dd17903652a210f9976c8ab18d9
Carlos Contreras <cdca12mx@gmail.com>
2019-04-05 22:18:20 -0700

    Añadido escuchador teclas
    Refactor

--- src/componentes/JComboFilter.java
+++ src/componentes/JComboFilter.java
@@ -3,11 +3,13 @@ package componentes;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.*;
 
 import javax.swing.*;
 
-public class JComboFilter extends JPanel implements ActionListener{
+public class JComboFilter extends JPanel implements ActionListener, KeyListener {
 
     private JComboBox<String> combo;
     private JPanel panelBotones;
@@ -16,6 +18,7 @@ public class JComboFilter extends JPanel implements ActionListener{
     private ComboBoxModel<String> comboModel;
     private Vector datosOriginal;
     private Vector datosOrdenado;
+
     public JComboFilter() {
         initInterface();
         hazEscuchas();
@@ -23,13 +26,12 @@ public class JComboFilter extends JPanel implements ActionListener{
 
     private void initInterface() {
         setLayout(new FlowLayout());
-        
+
         combo = new JComboBox<>();
 //        combo.addItem("Selecciona");
         combo.setPreferredSize(new Dimension(120, 40));
         combo.setEditable(true);
         add(combo);
-
 
         panelBotones = new JPanel(new GridLayout(0, 1, 0, 5));
         panelBotones.setPreferredSize(new Dimension(55, 40));
@@ -38,53 +40,74 @@ public class JComboFilter extends JPanel implements ActionListener{
         btnOrdenadoOriginal.setPreferredSize(new Dimension(20, 10));
         btnOrdenadoOriginal.setEnabled(false);
         panelBotones.add(btnOrdenadoOriginal);
-        
+
         btnOrdenadoAsc = new JButton("Ord");
         panelBotones.add(btnOrdenadoAsc);
 
         add(panelBotones);
-        
+
         datosOriginal = new Vector<String>();
-        
-    }
-    private void hazEscuchas() {
-    	btnOrdenadoAsc.addActionListener(this);
-    	btnOrdenadoOriginal.addActionListener(this);
-    }
-    
-    public void agrega(String dato) {
-    	combo.addItem(dato);
-    	datosOriginal.add(dato);
-    }
-    public void agrega(Vector datos) {
-    	comboModel=new DefaultComboBoxModel<>(datos);
-    	combo.setModel(comboModel);
-    	datosOriginal = datos;
-    }
-    private void ordenaDatos() {
-    	datosOrdenado = new Vector<String>(datosOriginal);
-    	Collections.sort(datosOrdenado);
+
     }
 
-	@Override
-	public void actionPerformed(ActionEvent evt) {
-		if(evt.getSource()==btnOrdenadoAsc){
-			System.out.println("llego1");
-			ordenaDatos();
-			comboModel = new DefaultComboBoxModel<>(datosOrdenado);
-			combo.setModel(comboModel);
-			btnOrdenadoAsc.setEnabled(false);
-			btnOrdenadoOriginal.setEnabled(true);
-			return;
-		}
-		if(evt.getSource()==btnOrdenadoOriginal){
-			System.out.println("llego2");
-			comboModel = new DefaultComboBoxModel<>(datosOriginal);
-			combo.setModel(comboModel);
-			btnOrdenadoOriginal.setEnabled(false);
-			btnOrdenadoAsc.setEnabled(true);
-			return;
-		}
-		
-	}
+    private void hazEscuchas() {
+        btnOrdenadoAsc.addActionListener(this);
+        btnOrdenadoOriginal.addActionListener(this);
+        combo.getEditor().getEditorComponent().addKeyListener(this);
+    }
+
+    public void agrega(String dato) {
+        combo.addItem(dato);
+        datosOriginal.add(dato);
+    }
+
+    public void agrega(Vector datos) {
+        comboModel = new DefaultComboBoxModel<>(datos);
+        combo.setModel(comboModel);
+        datosOriginal = datos;
+    }
+
+    private void ordenaDatos() {
+        datosOrdenado = new Vector<String>(datosOriginal);
+        Collections.sort(datosOrdenado);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        if (evt.getSource() == btnOrdenadoAsc) {
+            System.out.println("llego1");
+            ordenaDatos();
+            comboModel = new DefaultComboBoxModel<>(datosOrdenado);
+            combo.setModel(comboModel);
+            btnOrdenadoAsc.setEnabled(false);
+            btnOrdenadoOriginal.setEnabled(true);
+            return;
+        }
+        if (evt.getSource() == btnOrdenadoOriginal) {
+            System.out.println("llego2");
+            comboModel = new DefaultComboBoxModel<>(datosOriginal);
+            combo.setModel(comboModel);
+            btnOrdenadoOriginal.setEnabled(false);
+            btnOrdenadoAsc.setEnabled(true);
+            return;
+        }
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 }
