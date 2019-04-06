2d41115b7478a17c74aa5edc1dd8a26a567a90b2
Carlos Contreras <cdca12mx@gmail.com>
2019-04-06 16:44:48 -0700

    Test de componentes

--- src/componentes/FrameTest.java
+++ src/componentes/FrameTest.java
@@ -1,5 +1,8 @@
 package componentes;
 
+import java.awt.Color;
+import java.util.Vector;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.border.Border;
 
@@ -9,19 +12,39 @@ import javax.swing.border.Border;
  * @author Osiel Sainz
  */
 public class FrameTest extends JFrame {
-    
+
     JComboFilter comboFilter;
 
     public FrameTest() {
-        
+
         comboFilter = new JComboFilter();
-        comboFilter.setBounds(20, 20, 400, 150);
+        comboFilter.setBounds(20, 20, 200, 50);
         comboFilter.agrega("Osiel");
         comboFilter.agrega("Emanuel");
         comboFilter.agrega("Carlos");
         comboFilter.agrega("Daniel");
         comboFilter.agrega("Oscar");
         comboFilter.agrega("Paul");
+        comboFilter.agrega("Paul");
+        comboFilter.agrega("Paul");
+        comboFilter.agrega("Paul");
+        comboFilter.agrega("Paul");
+        comboFilter.setBorder(BorderFactory.createLineBorder(Color.RED));
+
+        JComboFilter nuevoComboFilter = new JComboFilter();
+        nuevoComboFilter.setBounds(20, 130, 200, 50);
+        Vector<String> vectorDatos = new Vector<String>();
+        vectorDatos.add("Osiel");
+        vectorDatos.add("Carlos");
+        vectorDatos.add("Osiel");
+        vectorDatos.add("Mario");
+        vectorDatos.add("Omar");
+        vectorDatos.add("Michel");
+        vectorDatos.add("Pero");
+        nuevoComboFilter.agrega(vectorDatos);
+        nuevoComboFilter.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        add(nuevoComboFilter);
+
         add(comboFilter);
         setLayout(null);
         setSize(400, 400);
@@ -30,7 +53,6 @@ public class FrameTest extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
-
 
     public static void main(String[] args) {
         new FrameTest();
