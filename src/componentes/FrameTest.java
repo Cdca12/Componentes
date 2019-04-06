366ae013617003e4d4ef988b61296ca13373cc87
Carlos Contreras <cdca12mx@gmail.com>
2019-04-05 19:38:25 -0700

    Se crean componentes de JComboFilter

--- src/componentes/FrameTest.java
+++ src/componentes/FrameTest.java
@@ -1,6 +1,7 @@
 package componentes;
 
 import javax.swing.JFrame;
+import javax.swing.border.Border;
 
 /**
  *
@@ -8,15 +9,26 @@ import javax.swing.JFrame;
  * @author Osiel Sainz
  */
 public class FrameTest extends JFrame {
-	
-	public FrameTest() {
-		setSize(300,400);
-		
+    
+    JComboFilter comboFilter;
 
-	}
+    public FrameTest() {
+        
+        comboFilter = new JComboFilter();
+        comboFilter.setBounds(20, 20, 400, 150);
+        add(comboFilter);
+        
+        setLayout(null);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
 
     public static void main(String[] args) {
-    	new FrameTest();  	
+        new FrameTest();
     }
-    
+
 }
--- src/componentes/JComboFilter.java
+++ src/componentes/JComboFilter.java
@@ -1,31 +1,44 @@
 package componentes;
+
 import java.awt.*;
 
 import javax.swing.*;
-public class JComboFilter extends JPanel{
-	private JComboBox<String> combo;
-	private JPanel panelBotones;
-	private JButton btnOrdenadoOriginal;
-	private JButton btnOrdenadoAsc;
-	public JComboFilter() {
-		
-		initInterface();
-	}
-	private void initInterface() {
-//		setLayout(new BorderLayout());
-		//inicializar combo
-		add(combo,BorderLayout.CENTER);
-		panelBotones = new JPanel(new GridLayout(0, 1, 0, 2));
-		btnOrdenadoOriginal = new JButton("Original");
-		panelBotones.add(btnOrdenadoOriginal);
-		
-		btnOrdenadoAsc = new JButton("Ordenado");
-		panelBotones.add(btnOrdenadoAsc);
-		
-		
-		
-		add(panelBotones, BorderLayout.EAST);
-		
-		
-	}
+
+public class JComboFilter extends JPanel {
+
+    private JComboBox<String> combo;
+    private JPanel panelBotones;
+    private JButton btnOrdenadoOriginal;
+    private JButton btnOrdenadoAsc;
+
+    public JComboFilter() {
+        initInterface();
+    }
+
+    private void initInterface() {
+        JPanel panelPrincipal = new JPanel(new FlowLayout());
+        add(panelPrincipal);
+        
+        combo = new JComboBox<>();
+        combo.addItem("Selecciona");
+        combo.setPreferredSize(new Dimension(120, 60));
+        combo.setEditable(true);
+        panelPrincipal.add(combo);
+
+
+        panelBotones = new JPanel(new GridLayout(0, 1, 0, 5));
+        panelBotones.setPreferredSize(new Dimension(100, 60));
+
+        btnOrdenadoOriginal = new JButton("Original");
+        btnOrdenadoOriginal.setPreferredSize(new Dimension(20, 10));
+        panelBotones.add(btnOrdenadoOriginal);
+
+        btnOrdenadoAsc = new JButton("Ordenado");
+        panelBotones.add(btnOrdenadoAsc);
+
+        panelPrincipal.add(panelBotones);
+        
+
+
+    }
 }
